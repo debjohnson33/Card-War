dc65dc97942f1b69f67a8e5d58590ee5e231f1c2
debjohnson33 <debjohnson_11@yahoo.com>
2018-09-22 14:55:05 -0500

    Added Hand class

--- card-war.py
+++ card-war.py
@@ -34,3 +34,12 @@ class Deck:
         single_card = self.deck.pop()
         return single_card
 
+class Hand:
+    def __init__(self):
+        self.cards =[]
+        self.value = 0
+
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
